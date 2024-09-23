d684fa7ecf6fb22905a354727397bba623cf45d7
RamnathSHA <s.h.a.ramnath@utwente.nl>
2024-09-23 14:33:45 +0200

    First local commit

---
+++ NWO api.py
@@ -0,0 +1,19 @@
+import requests as r
+import urllib.parse
+import json
+import pandas as pd
+
+## API adres
+rio_api = 'https://nwopen-api.nwo.nl/NWOpen-API/api/Projects'
+
+#rio_api = 'https://lod.onderwijsregistratie.nl/rio-api/api/v2'
+
+## GET Request
+result_api = r.get(rio_api)    #change based on API adress
+
+print(result_api.text)
+
+
+#results = json.loads(result_api.text)
+#print(results)
+
